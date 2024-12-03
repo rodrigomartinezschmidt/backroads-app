087aa86625eeb91cb14769ae0fce1ee0ae8c62de
Rodrigo Martinez <73548458+rodrigomartinezschmidt@users.noreply.github.com>
2024-12-03 10:09:52 +0100

    fix2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,7 +25,7 @@ const Footer = () => {
 								href={href}
 								target='_blank'
 								className='footer-icon'
-								rel='noreferrer'
+								rel='noopener noreferrer'
 							>
 								<i className={icon}></i>
 							</a>
